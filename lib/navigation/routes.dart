35c215fc051360219be64c14269f666648562bb4
xmshen <332016014@qq.com>
2023-06-19 10:48:26 +0800

    [init]: add routes, pages

---
+++ lib/navigation/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const String root = '/';
+  static const String productManagement = '/product-management';
+  static const String orderManagement = '/order-management';
+  static const String resourceManagement = '/resource-management';
+}
---
+++ lib/views/home-view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key, required this.title});
+  final String title;
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16),
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+        children: _buildGridTileList(4),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+List<Container> _buildGridTileList(int count) => List.generate(
+    count, (i) => Container(
+    padding: const EdgeInsets.all(6),
+    child:
+      const Center(
+        child: Text('12123123'),
+      )
+));
