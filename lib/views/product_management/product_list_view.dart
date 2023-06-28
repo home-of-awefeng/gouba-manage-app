4ac87f120b896d3181a14380d7b0c2a03867cba9
xmshen <332016014@qq.com>
2023-06-28 10:19:34 +0800

    [product]feat: add product list item view

--- lib/views/product_management/product_list_view.dart
+++ lib/views/product_management/product_list_view.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
 
 class ProductListView extends StatelessWidget {
-  ProductListView({Key? key}) : super(key: key);
+  const ProductListView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
         itemCount: 20,
         itemBuilder: (BuildContext context, int index) {
-          return SizedBox(
-            height: 50,
-            child: Center(child: Text('Entry ${index}')),
+          return Container(
+            margin: const EdgeInsets.only(top: 16),
+            // background:
+            child: Flex(
+              direction: Axis.horizontal,
+              children: [
+                SizedBox(
+                  width: 120,
+                  height: 120,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      ListTile(title: Text('Item#$index')),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextButton(
+                              child: const Text("下架"),
+                              onPressed: () {},
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextButton(
+                              child: const Text("修改"),
+                              onPressed: () {},
+                            ),
+                          )],
+                      ),
+                    ],
+                  )
+                ),
+            ]),
           );
         }
     );
